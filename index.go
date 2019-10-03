01e33915a303fe138196d0be05277e6ad4d9cfcf
blessyou <370732889@qq.com>
2019-10-03 15:04:48 +0800

    feat: 测试修改后的代码

--- index.go
+++ index.go
@@ -16,26 +16,25 @@ type Client struct {
 	AppPublicKeyCert    string // app公钥证书
 	AliPayPublicKeyCert string // 支付宝公钥证书
 	AliPayRootCert      string // 支付宝根证书
+	NotifyURL           string // 回调接口
+	SignType            string // 签名类型
 }
 
 // 预下单接口
-func (c *Client) TradePreCreate() {
+func (c *Client) TradePreCreate(p FaceToFacePayRequest) {
 	v := Params{
 		PublicRequest: PublicRequest{
 			AppID:     c.AppID,
 			Method:    AlipayTradePrecreateMethodName,
 			Format:    DefaultFormat,
 			Charset:   DefaultCharset,
-			SignType:  "RSA2",
+			SignType:  c.SignType,
 			Sign:      "",
+			NotifyURL: c.NotifyURL,
 			Timestamp: time.Now().Format(DefaultTimeFormat),
 			Version:   DefaultVersion,
 		},
-		FaceToFacePayRequest: FaceToFacePayRequest{
-			OutTradeNo:  "d88da8d8ad8a8d8a8d8",
-			Subject:     "测试支付",
-			TotalAmount: "1.11",
-		},
+		FaceToFacePayRequest: p,
 	}
 	// 获取签名
 	v.CommonPublicKeySign(c.AliPayPublicKey, c.AppPrivateKey)
